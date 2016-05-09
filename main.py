a5bda1a37121c4f34d9b0ba052e203f687ab5443
Kracav4ik <dikama2013@yandex.ru>
2016-05-10 00:46:24 +0300

    Пустое окно без ничего

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 # encoding: utf-8
 
-if __name__ == '__main__':
-    print('Hello, world')
+import pygame
+import sys
+
+
+pygame.init()
+
+WINDOW_SIZE = (1280, 720)  # размер окна в пикселах
+WINDOW_BG_COLOR = (0, 0, 0)  # цвет окна
+
+window_surface = pygame.display.set_mode(WINDOW_SIZE)
+
+
+def handle_input():
+    """Обработка input от игрока
+    """
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def process_game():
+    """Подвинуть игровые объекты
+    """
+
+
+def render():
+    """Отрисовка игры на экране
+    """
+    main_screen = pygame.display.get_surface()
+    main_screen.fill(WINDOW_BG_COLOR)
+
+    pygame.display.flip()
+
+
+# игровой цикл
+while True:
+    handle_input()
+    process_game()
+    render()
