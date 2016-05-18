283d5c2c35aed84ca3c881b164199f7f2716db77
Kracav4ik <dikama2013@yandex.ru>
2016-05-19 02:35:21 +0300

    Физика в отдельном файле

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pygame
 
 from background import Blackground
 from ball import Ball
-from collision import collide_circle_with_circle, collide_circle_with_border, collide_circle_with_poly
+from physics import PhysicsEngine
 from poly import Polygon
 from render import RenderManager, DebugText, DebugCursor
 from utils import Vec2d, random_vector, dot
@@ -46,48 +46,7 @@ def process_game(elapsed):
     # двигаем объекты
     phys_engine.process(elapsed)
 
-
-class PhysicsEngine:
-    def __init__(self, gravity):
-        self.gravity = gravity
-        self.bodies = []
-
-    def add_bodies(self, *bodies):
-        self.bodies.extend(bodies)
-
-    def process(self, elapsed):
-        for body in self.bodies:
-            if isinstance(body, Ball):
-                body.speed += 0.5 * self.gravity * elapsed
-                body.set_pos(body.pos + body.speed * elapsed)
-                body.speed += 0.5 * self.gravity * elapsed
-
-        collisions_list = []
-        for i1 in range(len(self.bodies)):
-            for i2 in range(i1 + 1, len(self.bodies)):
-                body1 = self.bodies[i1]
-                body2 = self.bodies[i2]
-                manifold = self.__collide_bodies(body1, body2)
-                if manifold:
-                    collisions_list.append(manifold)
-
-        for manifold in collisions_list:
-            manifold.collide()
-
-    @staticmethod
-    def __collide_bodies(body1, body2):
-        manifold = None
-        if not isinstance(body1, Ball):
-            body1, body2 = body2, body1
-
-        if isinstance(body1, Ball):
-            if isinstance(body2, Blackground):
-                manifold = collide_circle_with_border(body1, body2)
-            elif isinstance(body2, Polygon):
-                manifold = collide_circle_with_poly(body1, body2)
-            elif isinstance(body2, Ball):
-                manifold = collide_circle_with_circle(body1, body2)
-        return manifold
+    recolor()
 
 
 def recolor():
@@ -177,7 +136,6 @@ while True:
 
     handle_input()
     process_game(elapsed / 1000)
-    recolor()
     render_manager.render()
 
     frame_time = (time.time() - frame_start) * 1000
---
+++ physics.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+from background import Blackground
+from ball import Ball
+from collision import collide_circle_with_border, collide_circle_with_poly, collide_circle_with_circle
+from poly import Polygon
+
+
+class PhysicsEngine:
+    def __init__(self, gravity):
+        self.gravity = gravity
+        self.bodies = []
+
+    def add_bodies(self, *bodies):
+        self.bodies.extend(bodies)
+
+    def process(self, elapsed):
+        for body in self.bodies:
+            if isinstance(body, Ball):
+                body.speed += 0.5 * self.gravity * elapsed
+                body.set_pos(body.pos + body.speed * elapsed)
+                body.speed += 0.5 * self.gravity * elapsed
+
+        collisions_list = []
+        for i1 in range(len(self.bodies)):
+            for i2 in range(i1 + 1, len(self.bodies)):
+                body1 = self.bodies[i1]
+                body2 = self.bodies[i2]
+                manifold = self.__collide_bodies(body1, body2)
+                if manifold:
+                    collisions_list.append(manifold)
+
+        for manifold in collisions_list:
+            manifold.collide()
+
+    @staticmethod
+    def __collide_bodies(body1, body2):
+        manifold = None
+        if not isinstance(body1, Ball):
+            body1, body2 = body2, body1
+
+        if isinstance(body1, Ball):
+            if isinstance(body2, Blackground):
+                manifold = collide_circle_with_border(body1, body2)
+            elif isinstance(body2, Polygon):
+                manifold = collide_circle_with_poly(body1, body2)
+            elif isinstance(body2, Ball):
+                manifold = collide_circle_with_circle(body1, body2)
+        return manifold
